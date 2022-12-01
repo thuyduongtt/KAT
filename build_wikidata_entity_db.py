80bfddf674ea7bb79029bca3595ba9ef39756bf4
Duong Tran <d@dttt.io>
2022-12-01 23:43:54 +0100

    * Work with custom dataset

--- build_wikidata_entity_db.py
+++ build_wikidata_entity_db.py
@@ -179,7 +179,7 @@ def get_args_parser():
     parser.add_argument('--split_type', default='train', type=str)
     parser.add_argument('--qa_path', default='', type=str, help='./data/OpenEnded_mscoco_[split_type]_questions.json')
     parser.add_argument('--embedding_dir', type=str, default='./embeddings', help='dst root to faiss database')
-    parser.add_argument('--img_root', type=str, default='./test_images', help='img root to okvqa')
+    parser.add_argument('--img_root', type=str, default='./ds', help='img root to okvqa')
     parser.add_argument('--wikidata_ontology', type=str, default='./wikidata_ontology.pkl')
 
     return parser
